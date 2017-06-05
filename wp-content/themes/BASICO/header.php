adc736fe2c11c31370c120f7c63a97010d0144b7
carolingea <carolingea@live.com>
2017-06-04 23:54:34 -0500

    Menú

--- wp-content/themes/BASICO/header.php
+++ wp-content/themes/BASICO/header.php
@@ -100,7 +100,9 @@
             <!-- nav -->
             <nav class="nav menugeneral" role="navigation">
                 <?php //wp_nav_menu()  ?>
-                <?php wp_nav_menu( 'MenuPrincipal' ); ?>
+                <?php //wp_nav_menu( 'MenuPrincipal' ); ?>
+                <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
+                
 <!--                html5blank_nav();-->
             </nav>
 
